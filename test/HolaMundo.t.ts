188f2873f4c21f81cab3e51548339c133584bf24
kalchaqui <diego.rbarrionuevo@gmail.com>
2025-04-09 15:26:24 -0300

    first commit

---
+++ test/HolaMundo.t.ts
@@ -0,0 +1,10 @@
+console.log("Hola")
+
+function myFunction(){
+    const numero = 10;
+}
+
+describe("Test", myFunction)
+    /*function()()=> { //El describe tiene que tener nombre y funcion [()=> cumple la funcion de Function]*/
+
+ 
